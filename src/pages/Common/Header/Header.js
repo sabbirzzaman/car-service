d18fab0835c617c616423277b4179376263c633c
sabbirzzaman <sztonmoy7@gmail.com>
2022-04-13 01:00:37 +0600

    banner added

--- src/pages/Common/Header/Header.js
+++ src/pages/Common/Header/Header.js
@@ -1,19 +1,30 @@
 import React from 'react';
 import { Container, Nav, Navbar } from 'react-bootstrap';
+import logo from '../../../images/logo.png';
 
 const Header = () => {
     return (
         <>
             <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
                 <Container>
-                    <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-                    
+                    <Navbar.Brand href="#home">
+                        <img
+                            src={logo}
+                            height="30"
+                            className="d-inline-block align-top"
+                            alt="React Bootstrap logo"
+                        />
+                    </Navbar.Brand>
+
                     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                    <Navbar.Collapse className='header-nav' id="responsive-navbar-nav">
+                    <Navbar.Collapse
+                        className="header-nav"
+                        id="responsive-navbar-nav"
+                    >
                         <Nav>
-                            <Nav.Link href="#deets">More deets</Nav.Link>
-                            <Nav.Link eventKey={2} href="#memes">
-                                Dank memes
+                            <Nav.Link to="/home">Home</Nav.Link>
+                            <Nav.Link eventKey={2} to="/about">
+                                About
                             </Nav.Link>
                         </Nav>
                     </Navbar.Collapse>
