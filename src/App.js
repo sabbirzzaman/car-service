04d638f006f30b0c285dde864dd1316cfd8f0b66
sabbirzzaman <sztonmoy7@gmail.com>
2022-04-12 18:08:51 +0600

    firebase initialize

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import About from './pages/About/About/About';
+import Footer from './pages/Common/Header/Footer';
+import Header from './pages/Common/Header/Header';
+import Home from './pages/Home/Home/Home';
 
 function App() {
-  return (
-    <div>
-      
-    </div>
-  );
+    return (
+        <div>
+            <Header></Header>
+            <Routes>
+                <Route path="/" element={<Home></Home>}></Route>
+                <Route path="/about" element={<About></About>}></Route>
+            </Routes>
+            <Footer></Footer>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/pages/Home/Expats/Expats.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Expats = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Expats;
