c3609e880d0a8a383a8b9966d75eb3385c1eaeef
sabbirzzaman <sztonmoy7@gmail.com>
2022-04-13 01:30:53 +0600

    services section added

---
+++ src/pages/Home/Service/Service.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Button, Card } from 'react-bootstrap';
+
+const Service = ({ service }) => {
+    const { name, description, img } = service;
+
+    return (
+        <div className="col-lg-4 col-md-6 col-sm-6 col-12 g-4">
+            <div className="card">
+                <img src={img} className="card-img-top w-100" alt="" />
+                <div className="card-body">
+                    <h5 className="card-title">{name}</h5>
+                    <p className="card-text">{description}</p>
+                    <a href="https://reactjs.org/" className="btn btn-primary">
+                        Go somewhere
+                    </a>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Service;
--- src/pages/Home/Services/Services.js
+++ src/pages/Home/Services/Services.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Service from '../Service/Service';
 
 const Services = () => {
+    const [services, setServices] = useState([]);
+
+    useEffect(() => {
+        fetch('services.json')
+            .then((res) => res.json())
+            .then((data) => setServices(data));
+    }, []);
+
     return (
-        <div>
-            
+        <div className="container my-5">
+            <div className="title">
+                <h2>Our Services</h2>
+            </div>
+            <div className="row">
+                {services.map((service) => (
+                    <Service key={service.id} service={service}></Service>
+                ))}
+            </div>
         </div>
     );
 };
